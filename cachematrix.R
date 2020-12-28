7192cf6ada785597d9302ab5bc370382ebd4d589
Jessy <33096327+jessyshi@users.noreply.github.com>
2020-12-28 18:23:13 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -39,12 +39,3 @@ cacheSolve <- function(x, ...) {
         x$setinverse(invx)
         invx
 }
-
-A <- matrix( c(5, 1, 0,
-               3,-1, 2,
-               4, 0,-1), nrow=3, byrow=TRUE)
-
-#inv(A)
-
-
-makeCacheMatrix(A)
